0caf201c39c21fac7c705def657f50795e7bffa6
Sungrae-kogi <whvkek@naver.com>
2021-11-03 13:46:31 +0900

    로고화면다시짬, 프래그먼트 내부에 상하이동가능 분할 바생성

--- app/src/main/java/com/application/mob4git/MainActivity.java
+++ app/src/main/java/com/application/mob4git/MainActivity.java
@@ -6,14 +6,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.graphics.PointF;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     private FragmentManager fragmentManager = getSupportFragmentManager();
     private FragmentHome fragmentHome = new FragmentHome();
@@ -26,37 +32,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        //앱을 처음켰을때 대표적 화면 나오고 2.5초뒤에 사라지고 화면나오게하기
-        if (savedInstanceState == null) {
-            ImageView ImageView_Logo = findViewById(R.id.ImageView_Logo);
-            ImageView_Logo.setVisibility(View.VISIBLE);
-            // 네비게이션바 숨기기
-
-            BottomNavigationView BNView = findViewById(R.id.navigationView);
-            BNView.setVisibility(View.INVISIBLE);
-            // 툴바 숨기기
-            ActionBar actionBar = getSupportActionBar();
-            if (actionBar != null) {
-                actionBar.hide();
-            }
-
-            //화면 2.5초 나오게하고 사라지기
-            ImageView_Logo.postDelayed(() -> {     //delay뒤에 실행 할 코드
-                ImageView_Logo.setVisibility(View.INVISIBLE);
-                // 툴바,네비게이션뷰 보이게
-                if (actionBar != null) {
-                    actionBar.show();
-                    BNView.setVisibility(View.VISIBLE);
-                }
-            }, 2500);
-        }
-
-        //시작하자마자 나오는 홈 화면에 HomeFragme이 실행되게끔하는코드
+        //시작하자마자 나오는 홈 화면에 HomeFragment이 실행되게끔하는코드
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.replace(R.id.frameLayout, fragmentHome).commitAllowingStateLoss();
 
         BottomNavigationView BNView = findViewById(R.id.navigationView);
         BNView.setOnNavigationItemSelectedListener(new ItemSelectedListener());
+
+
+
     }
 
     class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener{
