add6979098ca34ee47e6eea6c44781595093c3dc
Sungrae-kogi <whvkek@naver.com>
2021-11-05 01:52:33 +0900

    액션바제거, 분할선과 버튼뷰의 움직임 동시화, 저장소,설정공간 분할선 제거

--- app/src/main/java/com/application/mob4git/FragmentSettings.java
+++ app/src/main/java/com/application/mob4git/FragmentSettings.java
@@ -5,48 +5,23 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class FragmentSettings extends Fragment implements View.OnTouchListener{
+public class FragmentSettings extends Fragment {
     View mainView;
-    View divider;
-    float oldYvalue;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mainView = inflater.inflate(R.layout.fragment_setting, container, false);
-        divider = mainView.findViewById(R.id.divider);
-        divider.setOnTouchListener(this);
 
         return mainView;
     }
 
 
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        int height = ((ViewGroup) v.getParent()).getHeight() - v.getHeight();
-
-        if(event.getAction() == MotionEvent.ACTION_DOWN){
-            oldYvalue = event.getY();
-        }else if(event.getAction() == MotionEvent.ACTION_MOVE){
-            v.setY(event.getRawY()-(oldYvalue+v.getHeight()));
-        }else if(event.getAction() == MotionEvent.ACTION_UP){
-            if(v.getY() > height-400){
-                v.setY(height-400);
-            }else if(v.getY() <400){
-                v.setY(400);
-            }else if(v.getY() <0 || v.getY() > height){
-                if(v.getY() < 0){
-                    v.setY(400);
-                }else{
-                    v.setY(height-400);
-                }
-            }
-        }
-        return true;
-    }
 }
